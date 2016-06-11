95f783edf1a8348382e5429d7d7c0b24129edce0
he he丶 <13889441200@163.com>
2016-06-11 18:41:57 +0800

    it can be auto updated

---
+++ app/README.md
@@ -0,0 +1,8 @@
+##`webpack` & `Vue`
+
+### This is a partly of a example of the vue project.
+
+```
+It can be run only after install necessary node modules.
+After that you just can visit http://localhost:8080 through 'npm start' command. 
+```
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,8 +1,14 @@
+var webpack = require("webpack"); 
+var path = require("path");
+//"webpack/hot/dev-server",
 module.exports = {
-	entry : "./src/main.js",
+	entry : {
+		main: [ "./src/main.js"] 
+	},
 	output : {
-		path : "./dist",
-		filename : "bundle.js"
+		path : path.join(__dirname, "./dist"),
+		filename : "bundle.js",
+		publicPath : "/dist/"
 	},
 	module : {
 		loaders : [
@@ -10,9 +16,25 @@ module.exports = {
 			{test : /\.vue$/, loader : "vue-loader"}
 		],
 	},
+	// .vue的配置。需要单独出来配置
 	vue : {
 		loaders : {
-			js : "babel"
+			js : "babel",
+			css: 'style!css!autoprefixer'
 		}
-	}
+	},
+	 // 转化成es5的语法
+	babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    },
+	plugins : [],
+	 // 服务器配置相关，自动刷新!
+    devServer: {
+        historyApiFallback: true,
+        hot: false,
+        inline: true,
+        grogress: true,
+    },
+    devtool: 'eval-source-map'
 }
